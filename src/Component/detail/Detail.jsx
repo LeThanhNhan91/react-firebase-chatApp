d8333a974285fed8619afc9ce30d23570b8d4806
LeThanhNhan91 <lethanhnhan09012004@gmail.com>
2024-07-27 01:47:38 +0700

    finish app layout

--- src/Component/detail/Detail.jsx
+++ src/Component/detail/Detail.jsx
@@ -2,8 +2,51 @@ import "./detail.css";
 
 const Detail = () => {
   return (
-    <div className='detail'>Detail</div>
-  )
-}
+    <div className="detail">
+      <div className="user">
+        <img src="./avatar.png" />
+        <h2>San San</h2>
+        <p>The apple of my eyes!</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat setting</span>
+            <img src="./arrowUp.png" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy & help</span>
+            <img src="./arrowUp.png" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared photos</span>
+            <img src="./arrowDown.png" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="https://ss-images.saostar.vn/w800/2024/6/21/pc/1718958819705/unozc4q2gn1-3y3f80f88l2-d10y1wt1ky3.jpg" />
+                <span>Conan Movie 26</span>
+              </div>
+              <img src="./download.png" className="icon"/>
+            </div>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowUp.png" />
+          </div>
+        </div>
+        <button>Block User</button>
+        <button className="logout">Logout</button>
+      </div>
+    </div>
+  );
+};
 
-export default Detail
+export default Detail;
